aae73d0024244372873cd0dff2ce395e8600e39f
Achim Bohnet <ach@mpe.mpg.de>
2007-02-20 19:01:08 +0000

    fix version 0.1.0 -> 0.1.0-beta1+svn
    svn path=/trunk/extragear/libs/libkdcraw/; revision=635727

--- version.h
+++ version.h
@@ -20,7 +20,7 @@
 #ifndef KDCRAW_VERSION_H
 #define KDCRAW_VERSION_H
 
-static const char kdcraw_version[] = "0.1.0";
+static const char kdcraw_version[] = "0.1.0-beta1+svn";
 
 #define KDCRAW_VERSION 0x000100
 
