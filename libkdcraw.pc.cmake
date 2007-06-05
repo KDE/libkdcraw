f6869beb027e9d4a3ad2ddcac0bc242e793706b2
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-05 08:43:15 +0000

    cmake port continue
    svn path=/trunk/extragear/libs/libkdcraw/; revision=671649

--- libkdcraw.pc.cmake
+++ libkdcraw.pc.cmake
@@ -1,12 +1,12 @@
-prefix=@prefix@
-exec_prefix=@exec_prefix@
-libdir=@libdir@
-includedir=@includedir@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}
 
 Name: libkdcraw
 Description: KDE interface library for dcraw command line program to decode RAW picture files 
 URL: http://www.kipi-plugins.org
 Requires:
 Version: 0.2.0
-Libs: -L${libdir} -lkdcraw
-Cflags: -I${includedir}
+Libs: -L${LIB_INSTALL_DIR} -lkdcraw
+Cflags: -I${INCLUDE_INSTALL_DIR}
