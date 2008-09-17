eb99d6a8dc5a46d9fffb44786f621d05813090ca
Gilles Caulier <caulier.gilles@gmail.com>
2008-09-17 03:55:40 +0000

    polish
    svn path=/trunk/KDE/kdegraphics/libs/libkdcraw/; revision=861739

--- libkdcraw.lsm.cmake
+++ libkdcraw.lsm.cmake
@@ -3,7 +3,7 @@ Title: libkdcraw
 Version: ${DCRAW_LIB_VERSION_STRING}
 Entered-date: 2007-05-05
 Description: A C++ wrapper around LibRaw library to decode RAW pictures. This library is used by digiKam and kipi-plugins.
-Keywords: kde image raw decode dcraw digikam kipi-plugins
+Keywords: kde image raw decode libraw digikam kipi-plugins
 Author: caulier dot gilles at gmail dot com (Gilles Caulier)
  marcel dot wiesweg at gmx dot de (Marcel Wiesweg)
 Maintained-by:
