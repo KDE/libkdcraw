9d12e49db7a9e7e0b41d75239ff15e1e8564fef7
Gilles Caulier <caulier.gilles@gmail.com>
2008-10-06 10:47:11 +0000

    add more raw image format descriptions
    svn path=/trunk/KDE/kdegraphics/libs/libkdcraw/; revision=868472

--- libkdcraw/rawfiles.h
+++ libkdcraw/rawfiles.h
@@ -34,10 +34,10 @@
 static const char raw_file_extentions[] = 
 
 // NOTE: VERSION 1
-                                          "*.bay "  // A specific Kodak picture format used by some high speed video cameras such as Kodak HRC-1000.
-                                          "*.bmq "  // 
-                                          "*.cr2 "  // Canon digital camera RAW image format version 2.0. These images are based on the TIFF image standard.
-                                          "*.crw "  // Canon digital camera RAW image format version 1.0. 
+                                          "*.bay "  // Casio Digital Camera Raw File Format.
+                                          "*.bmq "  // NuCore Raw Image File.
+                                          "*.cr2 "  // Canon Digital Camera RAW Image Format version 2.0. These images are based on the TIFF image standard.
+                                          "*.crw "  // Canon Digital Camera RAW Image Format version 1.0. 
                                           "*.cs1 "  // Capture Shop Raw Image File.
                                           "*.dc2 "  // Kodak DC25 Digital Camera File.
                                           "*.dcr "  // Kodak Digital Camera Raw Image Format for these models: Kodak DSC Pro SLR/c, Kodak DSC Pro SLR/n, Kodak DSC Pro 14N, Kodak DSC PRO 14nx.
@@ -45,42 +45,42 @@ static const char raw_file_extentions[] =
                                           "*.erf "  // Epson Digital Camera Raw Image Format.
                                           "*.fff "  // Imacon Digital Camera Raw Image Format.
                                           "*.hdr "  // Leaf Raw Image File.
-                                          "*.k25 "  // Kodak DC25 digital camera file.
-                                          "*.kdc "  // Photo Enhancer: Image Kodak Digital Camera, max 24 bits
-                                          "*.mdc "  // Minolta RD175 digital camera file.
-                                          "*.mos "  // Mamiya Digital Camera Raw Image File.
-                                          "*.mrw "  // Minolta Dimage Raw Image File.
-                                          "*.nef "  // Nikon Digital SLR Camera Raw Image File.
-                                          "*.orf "  // Olympus Digital Camera Raw Image File.
+                                          "*.k25 "  // Kodak DC25 Digital Camera Raw Image Format.
+                                          "*.kdc "  // Kodak Digital Camera Raw Image Format.
+                                          "*.mdc "  // Minolta RD175 Digital Camera Raw Image Format.
+                                          "*.mos "  // Mamiya Digital Camera Raw Image Format.
+                                          "*.mrw "  // Minolta Dimage Digital Camera Raw Image Format.
+                                          "*.nef "  // Nikon Digital Camera Raw Image Format.
+                                          "*.orf "  // Olympus Digital Camera Raw Image Format.
                                           "*.pef "  // Pentax Digital Camera Raw Image Format.
                                           "*.pxn "  // Logitech Digital Camera Raw Image Format.
-                                          "*.raf "  // Fuji CCD-RAW graphic file
+                                          "*.raf "  // Fuji Digital Camera Raw Image Format.
                                           "*.raw "  // Panasonic Digital Camera Image Format.
                                           "*.rdc "  // Digital Foto Maker Raw Image File.
-                                          "*.sr2 "  // Sony Digital Camera Raw Image File.
-                                          "*.srf "  // Sony Digital Camera Raw Image File for DSC-F828 8 megapixel digital camera or Sony DSC-R1
-                                          "*.x3f "  // Sigma Camera RAW Picture File for devices based on Foveon X3 direct image sensor.
-                                          "*.arw "  // Sony Digital Camera Raw Image File for Alpha devices.
+                                          "*.sr2 "  // Sony Digital Camera Raw Image Format.
+                                          "*.srf "  // Sony Digital Camera Raw Image Format for DSC-F828 8 megapixel digital camera or Sony DSC-R1
+                                          "*.x3f "  // Sigma Digital Camera Raw Image Format for devices based on Foveon X3 direct image sensor.
+                                          "*.arw "  // Sony Digital Camera Raw Image Format for Alpha devices.
 
 // NOTE: VERSION 2
 
-                                          "*.3fr "  // Hasselblad Digital Camera Raw Image File.
-                                          "*.cine " // Phantom software Raw Image File.
-                                          "*.ia "   // 
-                                          "*.kc2 "  // 
-                                          "*.mef "  // Mamiya Digital Camera Raw Image File.
-                                          "*.nrw "  // Nikon Digital Camera Raw Image File.
-                                          "*.qtk "  // Apple Quicktake 100/150 Camera Raw Image File.
-                                          "*.rw2 "  // Panasonic LX3 Camera Raw Image File.
+                                          "*.3fr "  // Hasselblad Digital Camera Raw Image Format.
+                                          "*.cine " // Phantom Software Raw Image File.
+                                          "*.ia "   // Sinar Raw Image File.
+                                          "*.kc2 "  // Kodak DCS200 Digital Camera Raw Image Format.
+                                          "*.mef "  // Mamiya Digital Camera Raw Image Format.
+                                          "*.nrw "  // Nikon Digital Camera Raw Image Format.
+                                          "*.qtk "  // Apple Quicktake 100/150 Digital Camera Raw Image Format.
+                                          "*.rw2 "  // Panasonic LX3 Digital Camera Raw Image Format.
                                           "*.sti";  // Sinar Capture Shop Raw Image File.
 
 /* TODO: check if these format are supported
                                           "*.drf "  // Kodak Digital Camera Raw Image Format.
                                           "*.dsc "  // Kodak Digital Camera Raw Image Format.
                                           "*.ptx "  // Pentax Digital Camera Raw Image Format.
-                                          "*.cap "  // Phase One Digital Camera Raw Image File.
-                                          "*.iiq "  // Phase One Digital Camera Raw Image File.
-                                          "*.rwz "  // Rawzor Digital Camera Raw Image File.
+                                          "*.cap "  // Phase One Digital Camera Raw Image Format.
+                                          "*.iiq "  // Phase One Digital Camera Raw Image Format.
+                                          "*.rwz "  // Rawzor Digital Camera Raw Image Format.
 */
 
 // increment this number whenever you change the above string
