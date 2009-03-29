94d56786350617dbb398fde216d8dc949b3a1afc
Andi Clemens <andi.clemens@gmx.net>
2009-03-29 22:31:45 +0000

    fix const iterator calls
    svn path=/trunk/KDE/kdegraphics/libs/libkdcraw/; revision=946649

--- libkdcraw/squeezedcombobox.cpp
+++ libkdcraw/squeezedcombobox.cpp
@@ -8,7 +8,7 @@
  *               content size
  *
  * Copyright (C) 2005 by Tom Albers <tomalbers@kde.nl>
- * Copyright (C) 2008 by Andi Clemens <andi dot clemens at gmx dot net>
+ * Copyright (C) 2008-2009 by Andi Clemens <andi dot clemens at gmx dot net>
  * Copyright (C) 2006-2009 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
@@ -120,7 +120,7 @@ void SqueezedComboBox::insertSqueezedItem(const QString& newItem, int index,
 
 void SqueezedComboBox::insertSqueezedList(const QStringList& newItems, int index)
 {
-    for(QStringList::const_iterator it = newItems.begin() ; it != newItems.end() ; ++it)
+    for(QStringList::const_iterator it = newItems.constBegin() ; it != newItems.constEnd() ; ++it)
     {
         insertSqueezedItem(*it, index);
         index++;
