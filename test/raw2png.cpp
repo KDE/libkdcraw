cafae783a4bd9f22944720a3b7aab9e4fca733e3
Gilles Caulier <caulier.gilles@gmail.com>
2010-07-12 20:26:23 +0000

    Doxygen like header
    svn path=/trunk/KDE/kdegraphics/libs/libkdcraw/; revision=1149177

--- test/raw2png.cpp
+++ test/raw2png.cpp
@@ -1,30 +1,32 @@
-/* ============================================================
+/** ===========================================================
  *
- * This file is a part of kipi-plugins project
- * http://www.kipi-plugins.org
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
  *
- * Date        : 2008-15-09
- * Description : a command line tool to convert RAW file to PNG
+ * @date   2008-15-09
+ * @brief  a command line tool to convert RAW file to PNG
  *
- * Copyright (C) 2008 by Gilles Caulier <caulier dot gilles at gmail dot com> 
+ * @author Copyright (C) 2008-2010 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
+ * either version 2, or (at your option)
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * ============================================================ */
 
 // Qt includes.
 
-#include <qstring.h>
-#include <qfile.h>
-#include <qfileinfo.h>
+#include <QString>
+#include <QFile>
+#include <QFileInfo>
 
 // KDE includes.
 
@@ -40,9 +42,9 @@
 
 using namespace KDcrawIface;
 
-int main (int argc, char **argv)
+int main (int argc, char** argv)
 {
-    if(argc != 2) 
+    if(argc != 2)
     {
         PRINT_DEBUG << "raw2png - RAW Camera Image to PNG Converter" ENDL;
         PRINT_DEBUG << "Usage: <rawfile>" ENDL;
