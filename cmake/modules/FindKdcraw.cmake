817112a6a0e2e38790b6099f4ab10ae6f256d35e
Michael Georg Hansen <mike@mghansen.de>
2010-12-13 21:16:42 +0000

    Fix copy/paste and polish.
    svn path=/branches/extragear/graphics/digikam/extra/libkdcraw/; revision=1206204

--- cmake/modules/FindKdcraw.cmake
+++ cmake/modules/FindKdcraw.cmake
@@ -22,7 +22,7 @@ if (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES AND KDCRAW_DEFINITIONS)
   message(STATUS "Found Kdcraw library in cache: ${KDCRAW_LIBRARIES}")
 
   # in cache already
-  SET(KDCRAW_FOUND TRUE)
+  set(KDCRAW_FOUND TRUE)
 
 else (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES AND KDCRAW_DEFINITIONS)
 
@@ -30,22 +30,21 @@ else (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES AND KDCRAW_DEFINITIONS)
 
   # Check if library is not in local sub-folder
 
-  IF (KDCRAW_LOCAL_DIR)
-    SET (KDCRAW_LOCAL_FOUND TRUE)
-  ELSE (KDCRAW_LOCAL_DIR)
-    FIND_FILE(KDCRAW_LOCAL_FOUND libkdcraw/kdcraw.h ${CMAKE_SOURCE_DIR}/libkdcraw ${CMAKE_SOURCE_DIR}/libs/libkdcraw NO_DEFAULT_PATH)
+  if (KDCRAW_LOCAL_DIR)
+    set (KDCRAW_LOCAL_FOUND TRUE)
+  else (KDCRAW_LOCAL_DIR)
+    find_file(KDCRAW_LOCAL_FOUND libkdcraw/kdcraw.h ${CMAKE_SOURCE_DIR}/libkdcraw ${CMAKE_SOURCE_DIR}/libs/libkdcraw NO_DEFAULT_PATH)
 
-    IF (KDCRAW_LOCAL_FOUND)
+    if (KDCRAW_LOCAL_FOUND)
       # Was it found in libkdcraw/ or in libs/libkdcraw?
-      FIND_FILE(KDCRAW_LOCAL_FOUND_IN_LIBS libkdcraw/kdcraw.h ${CMAKE_SOURCE_DIR}/libs/libkdcraw NO_DEFAULT_PATH)
-      IF (KDCRAW_LOCAL_FOUND_IN_LIBS)
-        SET(KDCRAW_LOCAL_DIR libs/libkdcraw)
-      ELSE (KDCRAW_LOCAL_FOUND_IN_LIBS)
-        SET(KDCRAW_LOCAL_DIR libkdcraw)
-      ENDIF (KDCRAW_LOCAL_FOUND_IN_LIBS)
-    ENDIF (KDCRAW_LOCAL_FOUND)
-  ENDIF (KDCRAW_LOCAL_DIR)
-  
+      find_file(KDCRAW_LOCAL_FOUND_IN_LIBS libkdcraw/kdcraw.h ${CMAKE_SOURCE_DIR}/libs/libkdcraw NO_DEFAULT_PATH)
+      if (KDCRAW_LOCAL_FOUND_IN_LIBS)
+        set(KDCRAW_LOCAL_DIR libs/libkdcraw)
+      else (KDCRAW_LOCAL_FOUND_IN_LIBS)
+        set(KDCRAW_LOCAL_DIR libkdcraw)
+      endif (KDCRAW_LOCAL_FOUND_IN_LIBS)
+    endif (KDCRAW_LOCAL_FOUND)
+  endif (KDCRAW_LOCAL_DIR)
 
   if (KDCRAW_LOCAL_FOUND)
 
@@ -56,45 +55,45 @@ else (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES AND KDCRAW_DEFINITIONS)
     set(KDCRAW_LIBRARIES kdcraw)
     message(STATUS "Found Kdcraw library in local sub-folder: ${CMAKE_SOURCE_DIR}/${KDCRAW_LOCAL_DIR}")
     set(KDCRAW_FOUND TRUE)
-    MARK_AS_ADVANCED(KDCRAW_INCLUDE_DIR KDCRAW_LIBRARIES KDCRAW_DEFINITIONS)
+    mark_as_advanced(KDCRAW_INCLUDE_DIR KDCRAW_LIBRARIES KDCRAW_DEFINITIONS)
 
   else(KDCRAW_LOCAL_FOUND)
 
     message(STATUS "Check Kdcraw library using pkg-config...")
-    if(NOT WIN32)
+    if (NOT WIN32)
       # use pkg-config to get the directories and then use these values
       # in the FIND_PATH() and FIND_LIBRARY() calls
-      INCLUDE(UsePkgConfig)
+      include(UsePkgConfig)
     
       PKGCONFIG(libkdcraw _KDCRAWIncDir _KDCRAWLinkDir _KDCRAWLinkFlags _KDCRAWCflags)
     
-      if(_KDCRAWLinkFlags)
+      if (_KDCRAWLinkFlags)
         # query pkg-config asking for a libkdcraw >= 0.2.0
-        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=0.2.0 libkdcraw RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
-        if(_return_VALUE STREQUAL "0")
+        exec_program(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=0.2.0 libkdcraw RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+        if (_return_VALUE STREQUAL "0")
             message(STATUS "Found libkdcraw release >= 0.2.0")
             set(KDCRAW_VERSION_GOOD_FOUND TRUE)
-        else(_return_VALUE STREQUAL "0")
+        else (_return_VALUE STREQUAL "0")
             message(STATUS "Found libkdcaw release < 0.2.0, too old")
             set(KDCRAW_VERSION_GOOD_FOUND FALSE)
             set(KDCRAW_FOUND FALSE)
-        endif(_return_VALUE STREQUAL "0")
-      else(_KDCRAWLinkFlags)
+        endif (_return_VALUE STREQUAL "0")
+      else (_KDCRAWLinkFlags)
         set(KDCRAW_VERSION_GOOD_FOUND FALSE)
         set(KDCRAW_FOUND FALSE)
-      endif(_KDCRAWLinkFlags)
-    ELSE(NOT WIN32)
+      endif (_KDCRAWLinkFlags)
+    else (NOT WIN32)
       set(KDCRAW_VERSION_GOOD_FOUND TRUE)
-    ENDif(NOT WIN32)
+    endif (NOT WIN32)
 
-    if(KDCRAW_VERSION_GOOD_FOUND)
+    if (KDCRAW_VERSION_GOOD_FOUND)
         set(KDCRAW_DEFINITIONS "${_KDCRAWCflags}")
     
-        FIND_PATH(KDCRAW_INCLUDE_DIR libkdcraw/version.h
+        find_path(KDCRAW_INCLUDE_DIR libkdcraw/version.h
         ${_KDCRAWIncDir}
         )
     
-        FIND_LIBRARY(KDCRAW_LIBRARIES NAMES kdcraw
+        find_library(KDCRAW_LIBRARIES NAMES kdcraw
         PATHS
         ${_KDCRAWLinkDir}
         )
@@ -102,7 +101,7 @@ else (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES AND KDCRAW_DEFINITIONS)
         if (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES)
             set(KDCRAW_FOUND TRUE)
         endif (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES)
-     endif(KDCRAW_VERSION_GOOD_FOUND) 
+     endif (KDCRAW_VERSION_GOOD_FOUND) 
      if (KDCRAW_FOUND)
          if (NOT Kdcraw_FIND_QUIETLY)
              message(STATUS "Found libkdcraw: ${KDCRAW_LIBRARIES}")
@@ -118,7 +117,7 @@ else (KDCRAW_INCLUDE_DIR AND KDCRAW_LIBRARIES AND KDCRAW_DEFINITIONS)
          endif (Kdcraw_FIND_REQUIRED)
      endif (KDCRAW_FOUND)
     
-    MARK_AS_ADVANCED(KDCRAW_INCLUDE_DIR KDCRAW_LIBRARIES KDCRAW_DEFINITIONS)
+    mark_as_advanced(KDCRAW_INCLUDE_DIR KDCRAW_LIBRARIES KDCRAW_DEFINITIONS)
 
   endif(KDCRAW_LOCAL_FOUND)
   
