e42f1cace80043a1069a41c3694dd9d6dea4be65
Gilles Caulier <caulier.gilles@gmail.com>
2013-09-07 10:04:45 +0200

    add config libraw header

---
+++ libraw/config-libraw.h.cmake
@@ -0,0 +1,35 @@
+/** ===========================================================
+ * @file
+ *
+ * This file is a part of digiKam project
+ * <a href="http://www.digikam.org">http://www.digikam.org</a>
+ *
+ * @date   2013-09-07
+ * @brief  Libraw config header
+ *
+ * @author Copyright (C) 2013 by Gilles Caulier
+ *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef CONFIG_LIBRAW_H
+#define CONFIG_LIBRAW_H
+
+/* Define to 1 if Libraw is compiled with OpenMP support */
+#cmakedefine LIBRAW_USE_OPENMP 1
+
+/* Define to 1 if Libraw is compiled with RawSpeed support */
+#cmakedefine LIBRAW_USE_RAWSPEED 1
+
+#endif /* CONFIG_LIBRAW_H */
