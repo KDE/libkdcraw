7e1fc995a42a9cfb1c393234d9c9ceeb377abe4c
Gilles Caulier <caulier.gilles@gmail.com>
2015-10-03 16:44:42 +0200

    do not use stylesheet

--- src/rwidgetutils.cpp
+++ src/rwidgetutils.cpp
@@ -41,6 +41,8 @@
 #include <QStandardPaths>
 #include <QVector>
 #include <QColorDialog>
+#include <QStyleOptionButton>
+#include <qdrawutil.h>
 
 // KDE includes
 
@@ -519,8 +521,6 @@ RColorSelector::RColorSelector(QWidget* const parent)
     : QPushButton(parent),
       d(new Private)
 {
-    setText(QString());
-    
     connect(this, SIGNAL(clicked()),
             this, SLOT(slotBtnClicked()));
 }
@@ -535,8 +535,7 @@ void RColorSelector::setColor(const QColor& color)
     if (color.isValid())
     {
         d->color = color;
-        setStyleSheet(QString::fromLatin1("background-color: rgb(%1, %2, %3); color: rgb(255, 255, 255)")
-                                          .arg(d->color.red()).arg(d->color.green()).arg(d->color.blue()));
+        update();
     }
 }    
 
@@ -553,6 +552,66 @@ void RColorSelector::slotBtnClicked()
     {
         setColor(color);
         emit signalColorSelected(color);
+    }
+}
+
+void RColorSelector::paintEvent(QPaintEvent*)
+{
+    QPainter painter(this);
+    QStyle* const style = QWidget::style();
+
+    QStyleOptionButton opt;
+
+    opt.initFrom(this);
+    opt.state    |= isDown() ? QStyle::State_Sunken : QStyle::State_Raised;
+    opt.features  = QStyleOptionButton::None;
+    opt.icon      = QIcon();
+    opt.text.clear();
+
+    style->drawControl(QStyle::CE_PushButtonBevel, &opt, &painter, this);
+
+    QRect labelRect = style->subElementRect(QStyle::SE_PushButtonContents, &opt, this);
+    int shift       = style->pixelMetric(QStyle::PM_ButtonMargin, &opt, this) / 2;
+    labelRect.adjust(shift, shift, -shift, -shift);
+    int x, y, w, h;
+    labelRect.getRect(&x, &y, &w, &h);
+
+    if (isChecked() || isDown())
+    {
+        x += style->pixelMetric(QStyle::PM_ButtonShiftHorizontal, &opt, this);
+        y += style->pixelMetric(QStyle::PM_ButtonShiftVertical,   &opt, this);
+    }
+
+    QColor fillCol = isEnabled() ? d->color : palette().color(backgroundRole());
+    qDrawShadePanel(&painter, x, y, w, h, palette(), true, 1, 0);
+    
+    if (fillCol.isValid())
+    {
+        const QRect rect(x + 1, y + 1, w - 2, h - 2);
+        
+        if (fillCol.alpha() < 255)
+        {
+            QPixmap chessboardPattern(16, 16);
+            QPainter patternPainter(&chessboardPattern);
+            patternPainter.fillRect(0, 0, 8, 8, Qt::black);
+            patternPainter.fillRect(8, 8, 8, 8, Qt::black);
+            patternPainter.fillRect(0, 8, 8, 8, Qt::white);
+            patternPainter.fillRect(8, 0, 8, 8, Qt::white);
+            patternPainter.end();
+            painter.fillRect(rect, QBrush(chessboardPattern));
+        }
+
+        painter.fillRect(rect, fillCol);
+    }
+
+    if (hasFocus())
+    {
+        QRect focusRect = style->subElementRect(QStyle::SE_PushButtonFocusRect, &opt, this);
+        QStyleOptionFocusRect focusOpt;
+        focusOpt.init(this);
+        focusOpt.rect            = focusRect;
+        focusOpt.backgroundColor = palette().background().color();
+        style->drawPrimitive(QStyle::PE_FrameFocusRect, &focusOpt, &painter, this);
     }
 }
 
