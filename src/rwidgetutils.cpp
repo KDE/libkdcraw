38c9fb092b8c29b157fd3cf1f78695dc403650f9
Gilles Caulier <caulier.gilles@gmail.com>
2015-10-07 23:24:49 +0200

    cleanup

--- src/rwidgetutils.cpp
+++ src/rwidgetutils.cpp
@@ -55,38 +55,6 @@
 namespace KDcrawIface
 {
 
-RActiveLabel::RActiveLabel(const QUrl& url, const QString& imgPath, QWidget* const parent)
-    : QLabel(parent)
-{
-    setMargin(0);
-    setScaledContents(false);
-    setOpenExternalLinks(true);
-    setTextFormat(Qt::RichText);
-    setFocusPolicy(Qt::NoFocus);
-    setTextInteractionFlags(Qt::LinksAccessibleByMouse);
-    setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
-    QImage img = QImage(imgPath);
-
-    updateData(url, img);
-}
-
-RActiveLabel::~RActiveLabel()
-{
-}
-
-void RActiveLabel::updateData(const QUrl& url, const QImage& img)
-{
-    QByteArray byteArray;
-    QBuffer    buffer(&byteArray);
-    img.save(&buffer, "PNG");
-    setText(QString::fromLatin1("<a href=\"%1\">%2</a>")
-            .arg(url.url())
-            .arg(QString::fromLatin1("<img src=\"data:image/png;base64,%1\">")
-            .arg(QString::fromLatin1(byteArray.toBase64().data()))));
-}
-
-// ------------------------------------------------------------------------------------
-
 RLineWidget::RLineWidget(Qt::Orientation orientation, QWidget* const parent)
     : QFrame(parent)
 {
