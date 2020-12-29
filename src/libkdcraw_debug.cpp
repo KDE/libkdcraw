43d862df9b17a4d5503953f58a09fd34396ed696
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-29 05:36:43 +0100

    Use ecm_qt_declare_logging_category, install libkdcraw.categories
    GIT_SILENT

--- src/libkdcraw_debug.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    SPDX-FileCopyrightText: 2014 Laurent Montel <montel at kde dot org>
-
-    SPDX-License-Identifier: LGPL-2.0-or-later
-*/
-
-#include "libkdcraw_debug.h"
-
-Q_LOGGING_CATEGORY(LIBKDCRAW_LOG, "libkdcraw")
